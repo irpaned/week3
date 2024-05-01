ab35ea7e63e5f31b73aeed958898ae4a048bb68b
irpaned <muhammadirfan2823@gmail.com>
2024-05-01 14:30:43 +0800

    menggunakan route

--- assets/Javascript/addProject.js
+++ assets/Javascript/addProject.js
@@ -1,111 +1,111 @@
-const dataProject = [];
+// const dataProject = [];
 
-function addProject (event) {
-    event.preventDefault();
+// function addProject (event) {
+//     event.preventDefault();
 
-    let projectName = document.getElementById("projectName").value;
-    let image = document.getElementById("image").files[0];
-    let imageURL = URL.createObjectURL(image)
-    let startDate = document.getElementById("startDate").value;
-    let endDate = document.getElementById("endDate").value;
-    let description = document.getElementById("description").value;
-
-
-    if (projectName == "") {
-        return alert ("Please enter your project name!")
-    } else if (startDate == "") {
-        return alert ("Please enter your Start Date!")
-    } else if (endDate == "") {
-        return alert ("Please enter your End Date!")
-    } else if (description == "") {
-        return alert ("Please enter your Description!")
-    } else if (imageURL == "") {
-        return alert ("Please upload your image!")
-    } 
-
-    if (endDate < startDate) {
-        return alert ("End date cannot be less than start date!")
-    }
+//     let projectName = document.getElementById("projectName").value;
+//     let image = document.getElementById("image").files[0];
+//     let imageURL = URL.createObjectURL(image)
+//     let startDate = document.getElementById("startDate").value;
+//     let endDate = document.getElementById("endDate").value;
+//     let description = document.getElementById("description").value;
 
 
-    let startDatePart = startDate.split("/")
-    let endDatePart = endDate.split("/")
+//     if (projectName == "") {
+//         return alert ("Please enter your project name!")
+//     } else if (startDate == "") {
+//         return alert ("Please enter your Start Date!")
+//     } else if (endDate == "") {
+//         return alert ("Please enter your End Date!")
+//     } else if (description == "") {
+//         return alert ("Please enter your Description!")
+//     } else if (imageURL == "") {
+//         return alert ("Please upload your image!")
+//     } 
 
-    let formatStartDate = startDatePart[2] + "-" + startDatePart[1] + "-" + startDatePart[0] 
-    let formatEndDate = endDatePart[2] + "-" + endDatePart[1] + "-" + endDatePart[0]   
-
-    let newStartDate = new Date(formatStartDate)
-    let newEndDate = new Date(formatEndDate)
-
-    let timeDifference = newEndDate - newStartDate
-
-    let differenceInDays = Math.floor(timeDifference / (1000 * 60 * 60 * 24)) 
-    let differenceInMonths = Math.floor(differenceInDays / 30.44) 
-    let differenceInYears = Math.floor(differenceInMonths / 12) 
-
-    let duration;
-
-    if(differenceInYears > 0) {
-        duration = `${differenceInYears} years`
-    } else if (differenceInMonths > 0) {
-        duration = `${differenceInMonths} months`
-    } else if (differenceInDays > 0) {
-        duration = `${differenceInDays} days`
-    }
+//     if (endDate < startDate) {
+//         return alert ("End date cannot be less than start date!")
+//     }
 
 
+//     let startDatePart = startDate.split("/")
+//     let endDatePart = endDate.split("/")
 
-    
-    dataProject.push(
-        {
-            projectName: projectName,
-            startDate: startDate,
-            endDate: endDate,
-            description: description,
-            image: imageURL,
-            duration: duration
+//     let formatStartDate = startDatePart[2] + "-" + startDatePart[1] + "-" + startDatePart[0] 
+//     let formatEndDate = endDatePart[2] + "-" + endDatePart[1] + "-" + endDatePart[0]   
 
-        }
-    )
+//     let newStartDate = new Date(formatStartDate)
+//     let newEndDate = new Date(formatEndDate)
+
+//     let timeDifference = newEndDate - newStartDate
+
+//     let differenceInDays = Math.floor(timeDifference / (1000 * 60 * 60 * 24)) 
+//     let differenceInMonths = Math.floor(differenceInDays / 30.44) 
+//     let differenceInYears = Math.floor(differenceInMonths / 12) 
+
+//     let duration;
+
+//     if(differenceInYears > 0) {
+//         duration = `${differenceInYears} years`
+//     } else if (differenceInMonths > 0) {
+//         duration = `${differenceInMonths} months`
+//     } else if (differenceInDays > 0) {
+//         duration = `${differenceInDays} days`
+//     }
+
+
 
     
-    console.log(dataProject)
+//     dataProject.push(
+//         {
+//             projectName: projectName,
+//             startDate: startDate,
+//             endDate: endDate,
+//             description: description,
+//             image: imageURL,
+//             duration: duration
 
-    newData()
+//         }
+//     )
 
-}
+    
+//     console.log(dataProject)
 
-function newData() {
-    document.getElementById("containerListProject").innerHTML = ""
+//     newData()
 
-    for(let i = 0; i < dataProject.length; i++) {
-        const projectSaya = dataProject[i]
+// }
 
-        document.getElementById("containerListProject").innerHTML +=`
-        <div class="card" style="width: 23rem; height: 580px;">
+// function newData() {
+//     document.getElementById("containerListProject").innerHTML = ""
+
+//     for(let i = 0; i < dataProject.length; i++) {
+//         const projectSaya = dataProject[i]
+
+//         document.getElementById("containerListProject").innerHTML +=`
+//         <div class="card" style="width: 23rem; height: 580px;">
         
-            <img src="${projectSaya.image}" class="card-img-top" alt="...">
+//             <img src="${projectSaya.image}" class="card-img-top" alt="...">
 
-            <div class="card-body mt-3">
-            <h5 class="card-title fw-bold" id="title">${projectSaya.projectName}</h5>
-            <p class="card-text">${projectSaya.startDate} - ${projectSaya.endDate}</p>
-            <p class="card-text">${projectSaya.duration}</p>
+//             <div class="card-body mt-3">
+//             <h5 class="card-title fw-bold" id="title">${projectSaya.projectName}</h5>
+//             <p class="card-text">${projectSaya.startDate} - ${projectSaya.endDate}</p>
+//             <p class="card-text">${projectSaya.duration}</p>
 
-            <div class="" style="height: 100px">
-                <p class="card-text" style="text-align: justify;" id="text">${projectSaya.description}</p>
-            </div>
+//             <div class="" style="height: 100px">
+//                 <p class="card-text" style="text-align: justify;" id="text">${projectSaya.description}</p>
+//             </div>
             
-            <div class="d-flex justify-content-between w-50 mt-3" >
-                <i class="bi bi-google-play fs-2"></i>
-                <i class="bi bi-android fs-2"></i>
-                <i class="fa-brands fa-java fs-2"></i>
-            </div>
+//             <div class="d-flex justify-content-between w-50 mt-3" >
+//                 <i class="bi bi-google-play fs-2"></i>
+//                 <i class="bi bi-android fs-2"></i>
+//                 <i class="fa-brands fa-java fs-2"></i>
+//             </div>
 
-            <div class=" mt-3 d-flex justify-content-around">
-                <button type="submit" class="btn btn-dark" style="width: 140px;">Edit</button>
-                <button type="submit" class="btn btn-dark" style="width: 140px;">Delete</button>
-            </div>
-            </div>
-        </div>`
-    }
-}
+//             <div class=" mt-3 d-flex justify-content-around">
+//                 <button type="submit" class="btn btn-dark" style="width: 140px;">Edit</button>
+//                 <button type="submit" class="btn btn-dark" style="width: 140px;">Delete</button>
+//             </div>
+//             </div>
+//         </div>`
+//     }
+// }
--- assets/Javascript/index.js
+++ assets/Javascript/index.js
@@ -1,18 +1,74 @@
-const express = require('express')
+const { log } = require("console");
+const express = require("express")
 const app = express()
 const port = 5000 // ctrl + c = untuk menghentikan port ketika ingin refresh(mengubah isi di res)
- 
+const path = require("path");
+const { title } = require("process");
+
+
 // app.set = setting variable global, configuration, etc
-app.set('view engine', 'hbs');
+app.set ("view engine", "hbs");
+// ini untuk mengasih tau bahwa file hbs nya ada difile views
+app.set("views", path.join(__dirname, "../../views"))
+
+// app.use = setting middleware
+app.use("/assets", express.static(path.join(__dirname, "../../assets")))
+
+// body parser
+app.use(express.urlencoded({extended : false}))
+// extended : false => querystring bawaan dari express
+// extended : true => querystring third party => qs
+
 // route = rute
 // untuk mengembalikan data(res) biasa menggunakan send atau json 
 // properties app = get, post, put, delete, patch
-app.get("/", home)
+app.get("/home", home)
+app.get("/addProject", addProject)
+app.post("/addProject", addTheProject)
+app.get("/contactMe", contactMe)
+app.get("/myProject", myProject)
+app.post("/myProject", myListProject)
+
+
+const data = [];
 
 // services
 function home (req, res) {
-    res.send("aaa")
+    res.render("home")  
 }
+
+function addProject (req, res) {
+    res.render("addProject" ,{data : data})  
+}
+
+function addTheProject (req, res) {
+    const {image ,title, content, startDate, endDate} = req.body
+    data.push({
+        image ,title, content, startDate, endDate
+    });
+    res.redirect("myProject")
+    console.log(data);
+}
+
+function contactMe (req, res) {
+    res.render("contactMe")  
+}
+
+function myProject (req, res) {
+    res.render("myProject", {data : data});  
+}
+
+function myListProject (req, res) {
+    
+    const {image ,title, content, startDate, endDate} = req.body
+    data.push({
+        image ,title, content, startDate, endDate
+    });
+    
+    // addTheProject()
+    
+}
+
 
 app.listen(port, () => {
     console.log("Server is running on PORT :", port);
