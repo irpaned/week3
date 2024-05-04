228089838e52d9c81b04343d62e918237edfa223
irpaned <muhammadirfan2823@gmail.com>
2024-05-05 05:03:15 +0800

    ada deh

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const app = express()
 const port = 5000 // ctrl + c = untuk menghentikan port ketika ingin refresh(mengubah isi di res)
 const path = require("path");
 const config = require("./config/config.json")
-const {Sequelize, QueryTypes} = require("sequelize");
+const {Sequelize, QueryTypes, where} = require("sequelize");
 const { log } = require("console");
+const { title } = require("process");
 const sequelize = new Sequelize(config.development)
-// const { title } = require("process");
+const bcrypt = require("bcrypt")
+const User = require("./models").user // INI CARA MEMAKAI ORM SELAIN PAKAI QUERY
+const session = require("express-session") // INI INSTALL NPM EXPRESS SESSION
+const flash = require("express-flash") // INSTALL 
 
 
 // app.set = setting variable global, configuration, etc
@@ -22,6 +26,20 @@ app.use("/assets", express.static(path.join(__dirname, "./assets")))
 app.use(express.urlencoded({extended : false}))
 // extended : false => querystring bawaan dari express
 // extended : true => querystring third party => qs
+
+// INI CARA MENGGUNAKAN EXPRESS SESSION YG KITA GUNAKAN
+app.use(session({
+    name : "mySession",
+    secret : "rahasiague",
+    resave: false,
+    saveUninitialized: true,
+    cookie : {
+        secure : false, //KENAPA false? KARENA KITA BELUM DEPLOY, MASIH DI LOCALHOST, KALAU DI HTTPS BARU PAKAI TRUE
+        maxAge : 1000 * 60 * 60 * 24 //CUMA BISA LOGIN 1 HARI, SETELAH ITU LOGIN ULANG
+    },
+}))
+
+app.use(flash());
 
 // route = rute
 // untuk mengembalikan data(res) biasa menggunakan send atau json 
@@ -45,15 +63,119 @@ app.post("/detail-Project", addDetailProject)
 app.get("/detailProject/:id", detailProjectView)
 
 
+app.get("/login", loginView)
+app.get("/register", registerView)
+
+app.post("/login", login)
+app.post("/register", register)
+
+app.post("/logout", logout)
+
 
 const data = [];
 
 // services
-function home (req, res) {
-    res.render("home")  
+
+// ------- LOGOUT ----------
+async function logout (req, res) {
+
+    // MEMBUAT LOGOUT DENGAN DESTROY EXPRESS-SESSION
+    req.session.destroy(function(err) {
+        if(err) return console.error("Logout failed!");
+
+        console.log("Logout succes!");
+        res.redirect("/login")
+    })
 }
 
-// ------ ADD PROJECT
+
+
+
+
+// ----------------- LOGIN ------------
+
+function loginView (req, res) { // UNTUK MUNCULKAN HALAMAN WEB
+    res.render("login")
+
+    
+}
+
+async function login (req, res) { // MENGAMBIL DATA DENGAN METHOD POST, DGN CARA DI PANGGIL
+    const { email, password } = req.body
+    console.log(email, password);
+
+
+    // INI CARA MENGGUNAKAN ORM SELAIN MEMAKAI QUERY
+    // BAGIAN const user = BISA KITA HAPUS KALAU GA BUTUH RETURN
+    const user = await User.findOne({
+        where: {email},
+    });
+
+    if(!user) {
+    //    return console.error("Email is not found!");
+
+       req.flash("error", "Email or password is wrong!")
+       return res.redirect("login")
+    };
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+    // console.log("is password valdi?", isPasswordValid);
+
+    if(!isPasswordValid) {
+        return console.error("Password is wrong!")
+
+    };
+
+    req.session.isLogin = true
+    req.session.user = {
+        name : user.name,
+        email : user.email
+    }
+
+    req.flash("succes", "Login berhasil!")
+    // console.log("Login berhasil!");
+    res.redirect("/home");
+    
+}
+
+// -------- REGISTER --------
+
+function registerView (req, res) {
+    res.render("register")
+}
+
+async function register (req, res) {
+    const {name, email, password} = req.body  // ini maksudnya adalah mengambil inputan data dari name="..." yg sudah diisi kan
+    // console.log(name, email, password);
+
+    // MENGUBAH PASSWORD MENJADI TULISAN RANDOM
+    const salt = 10; // SEMAKIN TINGGI ANGKANYA AKAN SEMAKIN SUSAH
+    const hashedPassword = await bcrypt.hash(password, salt);
+    // console.log("ini adalah password yang sudah kita hash", hashedPassword);
+
+    //MEMASUKKAN DATA KE TABLE USERS DI PG ADMIN
+    const query = `INSERT INTO users(name,email,password,"createdAt","updatedAt") VALUES('${name}', '${email}', '${hashedPassword}', now(), now())`
+    const data = await sequelize.query(query, {type : QueryTypes.INSERT})
+
+    // console.log(data);
+
+    res.redirect("/login")
+}
+
+// --------- HOME --------------
+
+function home (req, res) {
+
+    // isLogin dan user berfungsi untuk menghilangkan line apabila user login, liat bagian home.hbs untuk data yg dihilangkan
+    const isLogin = req.session.isLogin
+    const user = req.session.user
+
+    console.log(isLogin, user);
+    res.render("home",{isLogin, user});  
+    
+}
+
+// ------ ADD PROJECT --------
 
 function addProject (req, res) {
   res.render("addProject" ,{data})  
@@ -64,10 +186,10 @@ async function addTheProject (req, res) {
 
     const query = `INSERT INTO blogs(title,content,image,"createdAt","updatedAt") VALUES('${title}', '${content}','https://images.pexels.com/photos/1337373/pexels-photo-1337373.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2', now(), now())`
     const data = await sequelize.query(query, {type : QueryTypes.INSERT})
+    
+    // console.log("data yg di tambah", data);
 
-    console.log("data yg di tambah", data);
-
-    // console.log(title, content);
+    
     res.redirect("myProject");
     
 }
@@ -76,11 +198,12 @@ async function addTheProject (req, res) {
 // -------- DETAIL PROJECT ------------
 
 function addDetailProject (req, res) {
+
     const { title, content, endDate, startDate, id } = req.body;
     data [id] = {
         title, content, endDate, startDate, image
     }
-    res.redirect("myProject")
+    res.redirect("myProject")   
 }
 
 async function detailProjectView (req, res) {
@@ -90,7 +213,7 @@ async function detailProjectView (req, res) {
     const query = `SELECT * FROM blogs WHERE id=${id}` // Cara kedua
     const data = await sequelize.query(query, {type: QueryTypes.SELECT});
     console.log("data", data[0]);
-    res.render("detailProject" ,{data:data[0]}) 
+    // res.render("detailProject" ,{data:data[0]}) 
 }
 
 
@@ -107,23 +230,32 @@ async function deleteProject (req, res) {
 
 // ----------- EDIT PROJECT -----------
 
-function editProject (req, res) {
+async function editProject (req, res) {
 
     const { title, content, endDate, startDate, id } = req.body;
-    data [id] = {
-        title, content, endDate, startDate
-    }
+    // data [id] = {
+    //     title, content, endDate, startDate
+    // }
+    const query = `UPDATE blogs SET title='${title}', content='${content}' WHERE id=${id}`
+    const data = await sequelize.query(query, {type : QueryTypes.UPDATE})
+
+    // console.log(data);
+
     res.redirect("myProject")
 }
 
 
-function editProjectView (req, res) {
+async function editProjectView (req, res) {
 
     const { id } = req.params;
-    const selectedData = data[id];
-    selectedData.id = id
 
-    res.render("editProject", {data : selectedData});
+    const query = `SELECT * FROM blogs WHERE id=${id}` 
+    const data = await sequelize.query(query, {type: QueryTypes.SELECT});
+    
+    // const selectedData = data[id];
+    // selectedData.id = id
+
+    res.render("editProject", {data : data[0]});
 }
 
 
@@ -133,10 +265,12 @@ function contactMe (req, res) {
 }
 
 async function myProject (req, res) {
-    // res.render("myProject", {data : data});  
-
+    
+    // MENGAMBIL DATA DARI TABLE BLOGS YANG SUDAH DI ISI
     const query = "SELECT * FROM blogs"
     const data = await sequelize.query(query, {type: QueryTypes.SELECT});
+
+    // const data = 
 
     res.render("myProject" ,{data}) 
 
